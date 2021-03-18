d57c11a31c6c6aede9112400d5036774eed65522
yuzucha01 <78517616+yuzucha01@users.noreply.github.com>
2021-03-18 19:01:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -24,9 +24,7 @@
         <button class="google fb btn">Google+</button>
       </div>
     </form>
-
-
-
+    
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="js/app.js"></script>
